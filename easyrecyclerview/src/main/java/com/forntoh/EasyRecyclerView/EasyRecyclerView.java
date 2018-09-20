770d6e346ece50866a39f335907a9bb79c4684fa
forntoh <thomasforntoh@gmail.com>
2018-09-20 15:55:25 +0100

    initial commit

---
+++ easyrecyclerview/src/main/java/com/forntoh/EasyRecyclerView/EasyRecyclerView.java
@@ -0,0 +1,133 @@
+package com.forntoh.EasyRecyclerView;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.LinearSnapHelper;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SnapHelper;
+
+public class EasyRecyclerView {
+
+    private boolean dividers;
+    private int span = -1;
+    private Type type;
+    private RecyclerView rv;
+    private RecyclerView.Adapter adapter;
+    private InsetDecoration decoration;
+
+    public EasyRecyclerView() {
+    }
+
+    public EasyRecyclerView setAdapter(@NonNull RecyclerView.Adapter adapter) {
+        this.adapter = adapter;
+        return this;
+    }
+
+    public EasyRecyclerView setRecyclerView(@NonNull RecyclerView rv) {
+        this.rv = rv;
+        return this;
+    }
+
+    public EasyRecyclerView setType(@NonNull Type type) {
+        this.type = type;
+        return this;
+    }
+
+    public EasyRecyclerView setSpan(int span) {
+        this.span = span;
+        return this;
+    }
+
+    public EasyRecyclerView enableDividers(boolean dividers) {
+        this.dividers = dividers;
+        return this;
+    }
+
+    public void setItemSpacing(int itemSpacing, InsetDecoration.Sides sides) {
+        if (itemSpacing < 0)
+            throw new IndexOutOfBoundsException("Only positive values allowed");
+        this.decoration = new InsetDecoration(rv.getContext(), itemSpacing);
+        this.decoration.applyTo(sides);
+    }
+
+    public void initialize() {
+        if (adapter == null)
+            throw new NullPointerException("The adapter for the RecyclerView has not been defined : setAdapter(RecyclerView.Adapter adapter)");
+        else if (rv == null)
+            throw new NullPointerException("The RecyclerView has not been defined : setRecyclerView(RecyclerView rv)");
+        else
+            switch (type) {
+                case GRID:
+                    if (span < 0)
+                        throw new IllegalArgumentException("Grid span has not been defined : setSpan(int span)");
+                    else setupDisplay(type);
+                    break;
+                case VERTICAL:
+                    setupDisplay(type);
+                    break;
+                case HORIZONTAL:
+                    setupDisplay(type);
+                    break;
+                default:
+                    throw new EnumConstantNotPresentException(Type.class, "Type");
+            }
+    }
+
+    private void setupDisplay(Type type) {
+        RecyclerView.LayoutManager layoutManager;
+        switch (type) {
+            case GRID:
+                layoutManager = new GridLayoutManager(rv.getContext(), span);
+                if (dividers) {
+                    rv.addItemDecoration(new DividerItemDecoration(rv.getContext(), LinearLayoutManager.VERTICAL));
+                    rv.addItemDecoration(new DividerItemDecoration(rv.getContext(), LinearLayoutManager.HORIZONTAL));
+                }
+                break;
+            case VERTICAL:
+                layoutManager = new LinearLayoutManager(rv.getContext(), LinearLayoutManager.VERTICAL, false);
+                if (dividers)
+                    rv.addItemDecoration(new DividerItemDecoration(rv.getContext(), LinearLayoutManager.VERTICAL));
+                break;
+            default:
+                layoutManager = new LinearLayoutManager(rv.getContext(), LinearLayoutManager.HORIZONTAL, false);
+        }
+
+        rv.setLayoutManager(layoutManager);
+        rv.setItemAnimator(new DefaultItemAnimator());
+        rv.setAdapter(adapter);
+        rv.setNestedScrollingEnabled(false);
+        if (decoration != null) rv.addItemDecoration(decoration);
+    }
+
+    public Thread initialize(int listSize, int loopDuration, boolean snapToCenter) {
+        Thread thread;
+        if (snapToCenter) {
+            SnapHelper helper = new LinearSnapHelper();
+            helper.attachToRecyclerView(rv);
+        }
+        if (loopDuration > 0) {
+            thread = new Thread(() -> {
+                while (true) {
+                    try {
+                        LinearLayoutManager mL = (LinearLayoutManager) rv.getLayoutManager();
+                        int pos = mL.findLastCompletelyVisibleItemPosition();
+                        rv.smoothScrollToPosition(pos == listSize - 1 ? 0 : pos + 1);
+                        Thread.sleep(loopDuration);
+                    } catch (InterruptedException e) {
+                        break;
+                    }
+                }
+            });
+            thread.start();
+            thread.setName("Horizontal Scroll:" + rv.getId());
+        } else return null;
+        return thread;
+    }
+
+    public enum Type {
+        GRID, VERTICAL, HORIZONTAL
+    }
+}
---
+++ easyrecyclerview/src/main/java/com/forntoh/EasyRecyclerView/InsetDecoration.java
@@ -0,0 +1,72 @@
+package com.forntoh.EasyRecyclerView;
+
+import android.content.Context;
+import android.graphics.Rect;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.TypedValue;
+import android.view.View;
+
+public class InsetDecoration extends RecyclerView.ItemDecoration {
+
+    private int spacing;
+    private int left, right, top, bottom;
+    private Sides sides;
+
+    InsetDecoration(Context context, int inset) {
+        this.spacing = (int) dpToPixels(context, inset);
+    }
+
+    private static float dpToPixels(Context c, float value) {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, value, c.getResources().getDisplayMetrics());
+    }
+
+    void applyTo(Sides sides) {
+        this.sides = sides;
+    }
+
+    @Override
+    public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+        int position = parent.getChildViewHolder(view).getAdapterPosition();
+        int size = state.getItemCount();
+
+        RecyclerView.LayoutManager layoutManager = parent.getLayoutManager();
+        if (layoutManager instanceof GridLayoutManager) {
+            int cols = ((GridLayoutManager) layoutManager).getSpanCount();
+            left = position % cols != 1 ? spacing : spacing / 2;
+            right = position % cols != 0 ? spacing : spacing / 2;
+            top = position / cols == 0 ? spacing : spacing / 2;
+            bottom = spacing / 2;
+        } else if (layoutManager instanceof LinearLayoutManager) {
+            if (((LinearLayoutManager) layoutManager).getOrientation() == LinearLayoutManager.HORIZONTAL) {
+                left = position == 0 ? spacing : spacing / 2;
+                right = position == size - 1 ? spacing : spacing / 2;
+                top = spacing;
+                bottom = spacing;
+            } else {
+                left = spacing;
+                right = spacing;
+                top = position == 0 ? spacing : spacing / 2;
+                bottom = position == size - 1 ? spacing : spacing / 2;
+            }
+        }
+
+        if (sides == Sides.TOP_BOTTOM) {
+            left = 0;
+            right = 0;
+        } else if (sides == Sides.LEFT_RIGHT) {
+            top = 0;
+            bottom = 0;
+        }
+
+        outRect.left = left;
+        outRect.right = right;
+        outRect.top = top;
+        outRect.bottom = bottom;
+    }
+
+    public enum Sides {
+        TOP_BOTTOM, LEFT_RIGHT
+    }
+}
